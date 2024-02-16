5e8e7409ecfca32336cada1988df57ac95842844
Mathieu Pannetrat <mathieupannetrat5@gmail.com>
2024-02-16 01:11:02 +0100

    real fix, wait until the volume is mounted

--- dockerFiles/symfony/entrypoint.sh
+++ dockerFiles/symfony/entrypoint.sh
@@ -1,5 +1,25 @@
-#!/bin/sh
-sleep 10
+#!/bin/bash
+
+while [ ! -f "/my_project_directory/.env" ]; do
+    echo "Le fichier .env n'existe pas. Attente..."
+    sleep 1
+done
+
+while [ ! -d "/my_project_directory/config/packages/" ]; do
+    echo "Le répertoire /my_project_directory/config/packages/ n'existe pas. Attente..."
+    sleep 1
+done
+
+while [ ! -d "/my_project_directory/src/Controller/" ]; do
+    echo "Le répertoire /my_project_directory/src/Controller/ n'existe pas. Attente..."
+    sleep 1
+done
+
+while [ ! -d "/my_project_directory/templates/" ]; do
+    echo "Le répertoire /my_project_directory/templates/ n'existe pas. Attente..."
+    sleep 1
+done
+
 
 cd /my_project_directory
 sed -i 's|^DATABASE_URL="postgresql.*|DATABASE_URL="postgresql://postgres:toto@postgres:5432/db"|g' .env
@@ -15,5 +35,3 @@ cp index.html.twig /my_project_directory/templates/default/
 cd /my_project_directory
 
 symfony server:start
-
-exec "$@"
